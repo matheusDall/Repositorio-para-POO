9229d890402bf2efb931838f50e93b627e4112e7
Cebolão <120070763+matheusDall@users.noreply.github.com>
2024-03-05 22:06:24 -0300

    Add files via upload

---
+++ Atividade02.java
@@ -0,0 +1,24 @@
+public class Atividade02 {
+ 
+    public static void main(String[] args){
+
+        boolean varN1 = true;
+
+        int varN2 = 101;
+        long varN3 = 2L;
+        float varN4 = 9.9f;
+        double varN5 = 45.5;
+        char varN6 = 4;
+        byte varN7 = 2;
+        short varN8 = 72;
+
+        System.out.println(" boolean: " + varN1 
+        + "\n Int: \t" + varN2 
+        + " \n Long: " + varN3 
+        + " \n \"Float:\"" + varN4 
+        + "\n \\Double: " + varN5 
+        + "\n Char: " + (char)varN6 
+        + "\n Byte: " + varN7 
+        + "\n Short: " + varN8);
+       }
+} 
